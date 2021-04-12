871775e664b9bbc3bc000f9163d8234bb720a711
Crystal <ceceherrera490@gmail.com>
2021-04-12 01:08:49 -0400

    commit tree 2b3c29f872d9e6b4a9c2aafa986e9128698469f9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function ([drivr1, drivr2]) {
+  let newArray = ["Sally", "Bob"];
+  return newArray;
+};
+
+const returnLastTwoDrivers = function ([drivr1, drivr2]) {
+  let newArray = ["Freddy", "Claudia"];
+  return newArray;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integr) {
+  return function fareMultiplier(fare) {
+    return integr * fare;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driversSelected) {
+  return driversSelected(drivers);
+}
